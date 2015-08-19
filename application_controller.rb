676d537d8489c775f7a7e233e47be7a70b370fc3
dfenjves <fenjves.daniel@gmail.com>
2015-08-19 19:35:44 -0400

    revamp. this is a terrible commit message

--- application_controller.rb
+++ application_controller.rb
@@ -4,6 +4,7 @@ Dotenv.load
 require 'pry'
 
 require_relative 'models/legislator_finder.rb'
+require_relative 'models/letter_builder.rb'
 
 class ApplicationController < Sinatra::Base
   enable :sessions
@@ -14,20 +15,24 @@ class ApplicationController < Sinatra::Base
 
   post '/' do
     @repinfo = LegislatorFinder.new.find_by_zip(params[:zip])
-    puts @repinfo
     erb :representatives
   end
 
   post '/representatives' do
-    @chosen_reps = []
-    params.each_key do |bioguide_id|
-    @chosen_reps << LegislatorFinder.new.find_by_bioguide_id(bioguide_id)
-    end
-    erb :template
+    rep = LegislatorFinder.new.find_by_bioguide_id(params[:bioguide_id])
+    @letter = LetterBuilder.new(rep)
+    erb :write
   end
 
   post '/build' do
-    @letter = LetterBuilder.new()
+    rep = LegislatorFinder.new.find_by_bioguide_id(params[:bioguide_id])
+    @letter = LetterBuilder.new(rep)
+    @letter.content = params[:content]
+    @letter.writer_name = params[:writer_name]
+    @letter.writer_address = params[:writer_address]
+    @letter.writer_phone = params[:writer_phone]
+    @letter.writer_email = params[:writer_email]
+    erb :review
   end
 
 end
--- models/letter_builder.rb
+++ models/letter_builder.rb
@@ -1,44 +1,38 @@
 class LetterBuilder
-  attr_reader :representative, :from, :user_name, :user_address,:user_phone, :user_email, :support_or_opposition, :issue, :who_am_i, :affected_parties, :primary_concern, :details, :outcome
+  attr_accessor :representative, :content, :writer_name, :writer_address,:writer_phone, :writer_email
 
-  def initialize(bioguide_id)
-    @representative = LegislatorFinder.new.search_by_bioguide_id(bioguide_id)
+  def initialize(representative)
+    @representative = representative
+  end
+
+  def bioguide_id
+    @representative['bioguide_id']
   end
 
 
   def to_address
-    "The Honorable #{representative['first_name']} #{representative['last_name']}
-     #{representative['office']}
-     Washington, DC 20510
+    "<p>The Honorable #{representative['first_name']} #{representative['last_name']}</p>
+     <p>#{representative['office']}</p>
+     <p>Washington, DC 20510</p>
     "
   end
 
   def from
-    @from = "#{@user_name}
-            #{@user_address}
-            #{@user_phone}
-            #{@user_email}"
+    @from = "#{@writer_name}
+            #{@writer_address}
+            #{@writer_phone}
+            #{@writer_email}"
   end
 
   def salutation
-    if @representative["title"] == "Sen"
+    if @representative["title"] == "sen"
       "Dear Senator #{representative['last_name']},"
-    elsif @representative["title"] == "Rep" && @representative["gender"] == "F"
+    elsif @representative["title"] == "rep" && @representative["gender"] == "F"
       "Dear Congresswoman #{representative['last_name']},"
     else
       "Dear Congressman #{representative['last_name']},"
     end    
   end
 
-  def content
-    "I write to you as a constituent expressing my #{support_or_opposition} the upcoming legislation addressing #{@issue}. This issue is of paramount interest to me because I am #{@who_am_i}. This issue directly impacts #{@affected_parties}.
-
-    I am primarily concerned about #{@primary_concern} because #{@details}.
-
-    Thank you for your consideration of my viewpoint on this matter. I believe it is an important issue, and would like to see the legislation #{@outcome}.
-
-    Sincerely,
-    "
-  end
 
 end
